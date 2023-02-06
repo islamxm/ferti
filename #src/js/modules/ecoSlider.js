d054e9536a2a567b96be165c52399ed918fc23b8
Islam <isxmad@gmail.com>
2023-02-06 10:00:20 +0300

    container size

--- #src/js/modules/ecoSlider.js
+++ #src/js/modules/ecoSlider.js
@@ -5,12 +5,26 @@ const ecoSlider = () => {
 
     if(el) {
         const slider = new Swiper(el, {
-            slidesPerView: 4,
+            slidesPerView: 1,
             spaceBetween: 26,
             modules: [Navigation],
             navigation: {
                 prevEl: '.ecoParts__body_slider_nav_item-prev',
                 nextEl: '.ecoParts__body_slider_nav_item-next'
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 4,
+                    spaceBetween: 26,
+                },
+                520: {
+                    slidesPerView: 3,
+                    spaceBetween: 26,
+                },
+                400: {
+                    slidesPerView: 2,
+                    spaceBetween: 26,
+                }
             }
         })
     }
