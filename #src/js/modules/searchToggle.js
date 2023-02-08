964e21f94f417ec2365373c852460bfc6eebdf6e
Islam Madaminov <isxmad@gmail.com>
2023-02-09 00:33:16 +0300

    header

--- #src/js/modules/searchToggle.js
+++ #src/js/modules/searchToggle.js
@@ -1,9 +1,10 @@
 const searchToggle = () => {
     const btn = document.querySelectorAll('.header__bottom_search_btn');
     const body = document.querySelectorAll('.header__bottom_search_body');
-    const wrapper = document.querySelectorAll('.header__bottom_in');
+    const wrapper = document.querySelectorAll('.header__top_in');
     const mobWrap = document.querySelector('.mobmenu__head_main_search');
-    if(wrapper && btn && body) {
+    const sr = document.querySelectorAll('.header__bottom_search');
+    if(wrapper && btn?.length > 0 && body && sr?.length > 0) {
         btn.forEach(bt => {
             bt.addEventListener('click', () => {
                 wrapper.forEach(i => {
@@ -12,7 +13,18 @@ const searchToggle = () => {
                 mobWrap.classList.toggle('active')
             })
         })
-        
+
+        sr.forEach(i => {
+            i.addEventListener('click', (e) => {
+                if(e.target.classList.contains('header__bottom_search_layer')) {
+                    wrapper.forEach(i => {
+                        i.classList.remove('search');
+                    })
+                    mobWrap.classList.remove('active')
+                }
+            } )
+        })
+       
     }
     
 
